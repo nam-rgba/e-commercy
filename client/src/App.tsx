2ed253991850ac02fabf9d147cb33f150db503c4
Nam <doanngocnam3002@gmail.com>
2024-01-11 13:42:15 +0700

    auth setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import  {lazy, Suspense} from 'react'
+
+import Home from "./pages/Home"
+const LayoutAccount = lazy(() => import('./pages/Auth/LayoutAccount'))
+const SignIn = lazy(() => import('./pages/Auth/SignIn'))
+const SingUp = lazy(() => import('./pages/Auth/SingUp'))
+
+
 
 function App() {
-
-
   return (
-    <>
-
-    </>
+    <BrowserRouter>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="account" element={<LayoutAccount />} >
+            <Route path="login" element={<SignIn />} />
+            <Route path="register" element={<SingUp/>} />
+          </Route>
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
   )
 }
 
